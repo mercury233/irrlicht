aeda8c7eeb55d52964fc7dc6116cd45da9a401fc
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2023-07-27 19:23:49 +0000

    Backport parts of COpenGLCommon.h to ensure that COpenGLExtensionHandler, COpenGLShaderMaterialRenderer and COpenGLTexture all use identical GL extension settings This has been improved in svn trukn a few years ago, but Irrlicht 1.8 still used a wild mix. Note: I did not change the cg parts (just left them as they are as I'm not going to start testing that) I guess slight risk of messing up something on some platform, but should in general improve things.
    git-svn-id: https://svn.code.sf.net/p/irrlicht/code/branches/releases/1.8@6508 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/COpenGLExtensionHandler.h
+++ source/Irrlicht/COpenGLExtensionHandler.h
@@ -12,71 +12,7 @@
 #include "irrTypes.h"
 #include "os.h"
 
-#if defined(_IRR_WINDOWS_API_)
-	// include windows headers for HWND
-	#define WIN32_LEAN_AND_MEAN
-	#include <windows.h>
-	#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
-		#define GL_GLEXT_LEGACY 1
-	#endif
-	#include <GL/gl.h>
-	#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
-		#include "glext.h"
-	#endif
-	#include "wglext.h"
-
-	#ifdef _MSC_VER
-		#pragma comment(lib, "OpenGL32.lib")
-	#endif
-
-#elif defined(_IRR_COMPILE_WITH_OSX_DEVICE_)
-	#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
-		#define GL_GLEXT_LEGACY 1
-	#endif
-	#include <OpenGL/gl.h>
-	#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
-		#include "glext.h"
-	#endif
-#elif defined(_IRR_COMPILE_WITH_SDL_DEVICE_) && !defined(_IRR_COMPILE_WITH_X11_DEVICE_)
-	#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
-		#define GL_GLEXT_LEGACY 1
-		#define GLX_GLXEXT_LEGACY 1
-	#else
-		#define GL_GLEXT_PROTOTYPES 1
-		#define GLX_GLXEXT_PROTOTYPES 1
-	#endif
-	#define NO_SDL_GLEXT
-	#include <SDL/SDL_video.h>
-	#include <SDL/SDL_opengl.h>
-	#include "glext.h"
-#else
-	#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
-		#define GL_GLEXT_LEGACY 1
-		#define GLX_GLXEXT_LEGACY 1
-	#else
-		#define GL_GLEXT_PROTOTYPES 1
-		#define GLX_GLXEXT_PROTOTYPES 1
-	#endif
-	#include <GL/gl.h>
-	#include <GL/glx.h>
-	#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
-	#include "glext.h"
-	#undef GLX_ARB_get_proc_address // avoid problems with local glxext.h
-	#include "glxext.h"
-	#endif
-#endif
-
-#ifndef GL_ARB_shader_objects
-/* GL types for program/shader text and shader object handles */
-typedef char GLcharARB;
-typedef unsigned int GLhandleARB;
-#endif
-
-#ifndef GL_VERSION_2_0
-/* GL type for program/shader text */
-typedef char GLchar;
-#endif
-
+#include "COpenGLCommon.h"
 
 namespace irr
 {
--- source/Irrlicht/COpenGLTexture.h
+++ source/Irrlicht/COpenGLTexture.h
@@ -11,33 +11,7 @@
 #include "IrrCompileConfig.h"
 #ifdef _IRR_COMPILE_WITH_OPENGL_
 
-#if defined(_IRR_OPENGL_USE_EXTPOINTER_)
-	#define GL_GLEXT_LEGACY 1
-#else
-	#define GL_GLEXT_PROTOTYPES 1
-#endif
-#ifdef _IRR_WINDOWS_API_
-	// include windows headers for HWND
-	#define WIN32_LEAN_AND_MEAN
-	#include <windows.h>
-	#include <GL/gl.h>
-#ifdef _MSC_VER
-	#pragma comment(lib, "OpenGL32.lib")
-#endif
-#elif defined(_IRR_OSX_PLATFORM_)
-	#include <OpenGL/gl.h>
-#elif defined(_IRR_COMPILE_WITH_SDL_DEVICE_)
-	#define NO_SDL_GLEXT
-	#include <SDL/SDL_video.h>
-	#include <SDL/SDL_opengl.h>
-#else
-	#if defined(_IRR_OSX_PLATFORM_)
-		#include <OpenGL/gl.h>
-	#else
-		#include <GL/gl.h>
-	#endif
-#endif
-
+#include "COpenGLCommon.h"
 
 namespace irr
 {
@@ -202,4 +176,3 @@ protected:
 
 #endif
 #endif // _IRR_COMPILE_WITH_OPENGL_
-

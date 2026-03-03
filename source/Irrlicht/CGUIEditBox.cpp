206d36686d0da5287c4a9c761a6ae26baec17dd2
mercury233 <me@mercury233.me>
2026-03-03 14:47:38 +0800

    don't send EGET_EDITBOX_CHANGED when no char is inputed

--- source/Irrlicht/CGUIEditBox.cpp
+++ source/Irrlicht/CGUIEditBox.cpp
@@ -1459,10 +1459,10 @@ void CGUIEditBox::inputChar(wchar_t c)
 			BlinkStartTime = os::Timer::getTime();
 			setTextMarkers(0, 0);
 		}
+		breakText();
+		calculateScrollPos();
+		sendGuiEvent(EGET_EDITBOX_CHANGED);
 	}
-	breakText();
-	calculateScrollPos();
-	sendGuiEvent(EGET_EDITBOX_CHANGED);
 }
 
 // calculate autoscroll

397d1fbf3c6072219427ad0dcb41046e7a679000
mercury233 <me@mercury233.me>
2026-03-18 20:34:21 +0800

    fix comment of IImage::lock and IImage::unlock

--- include/IImage.h
+++ include/IImage.h
@@ -192,19 +192,17 @@ public:
 	}
 
 	//! Lock function. Use this to get a pointer to the image data.
-	/** Use getData instead.
+	/** Deprecated in [r5168]. Use getData instead.
 	\return Pointer to the image data. What type of data is pointed to
 	depends on the color format of the image. For example if the color
-	format is ECF_A8R8G8B8, it is of u32. Be sure to call unlock() after
-	you don't need the pointer any more. */
+	format is ECF_A8R8G8B8, it is of u32. */
 	IRR_DEPRECATED void* lock()
 	{
 		return getData();
 	}
 
 	//! Unlock function.
-	/** Should be called after the pointer received by lock() is not
-	needed anymore. */
+	/** Deprecated in [r5168]. */
 	IRR_DEPRECATED void unlock()
 	{
 	}

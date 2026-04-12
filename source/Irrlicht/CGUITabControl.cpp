7848b7ccd13b2a2149fbc8d2bf0a003b760ff7b1
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2026-04-12 14:45:38 +0000

    Bugfix: CGUITabControl can hide control buttons again when resizing element when it was scrolled already Thanks @Mercury233 for the patch (#332 - 6) Previously when the tab control was scrolled to the right then it could no longer hide the scroll buttons even if they were no longer needed
    git-svn-id: https://svn.code.sf.net/p/irrlicht/code/branches/releases/1.8@6804 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/CGUITabControl.cpp
+++ source/Irrlicht/CGUITabControl.cpp
@@ -792,7 +792,9 @@ void CGUITabControl::recalculateScrollBar()
 	if (!UpButton || !DownButton)
 		return;
 
-	ScrollControl = needScrollControl() || CurrentScrollTabIndex > 0;
+	ScrollControl = needScrollControl();
+	if (!ScrollControl)
+		CurrentScrollTabIndex = 0;
 
 	if (ScrollControl)
 	{
